6f7a74f8a46b7308acae08b22d6d30586efcdd40
liqidecg <128770480+liqidecg@users.noreply.github.com>
2024-06-04 22:50:12 +0800

    Update Keybox.java

--- app/src/main/java/com/android/internal/util/framework/Keybox.java
+++ app/src/main/java/com/android/internal/util/framework/Keybox.java
@@ -6,105 +6,92 @@ import org.lsposed.lsparanoid.Obfuscate;
 public final class Keybox {
     public static final class EC {
         public static final String PRIVATE_KEY = """
-                -----BEGIN EC PRIVATE KEY-----
-                MHcCAQEEICHghkMqFRmEWc82OlD8FMnarfk19SfC39ceTW28QuVEoAoGCCqGSM49
-                AwEHoUQDQgAE6555+EJjWazLKpFMiYbMcK2QZpOCqXMmE/6sy/ghJ0whdJdKKv6l
-                uU1/ZtTgZRBmNbxTt6CjpnFYPts+Ea4QFA==
-                -----END EC PRIVATE KEY-----
+           -----BEGIN EC PARAMETERS-----
+           BggqhkjOPQMBBw==
+           -----END EC PARAMETERS-----
                 """;
         public static final String CERTIFICATE_1 = """
-                -----BEGIN CERTIFICATE-----
-                MIICeDCCAh6gAwIBAgICEAEwCgYIKoZIzj0EAwIwgZgxCzAJBgNVBAYTAlVTMRMw
-                EQYDVQQIDApDYWxpZm9ybmlhMRYwFAYDVQQHDA1Nb3VudGFpbiBWaWV3MRUwEwYD
-                VQQKDAxHb29nbGUsIEluYy4xEDAOBgNVBAsMB0FuZHJvaWQxMzAxBgNVBAMMKkFu
-                ZHJvaWQgS2V5c3RvcmUgU29mdHdhcmUgQXR0ZXN0YXRpb24gUm9vdDAeFw0xNjAx
-                MTEwMDQ2MDlaFw0yNjAxMDgwMDQ2MDlaMIGIMQswCQYDVQQGEwJVUzETMBEGA1UE
-                CAwKQ2FsaWZvcm5pYTEVMBMGA1UECgwMR29vZ2xlLCBJbmMuMRAwDgYDVQQLDAdB
-                bmRyb2lkMTswOQYDVQQDDDJBbmRyb2lkIEtleXN0b3JlIFNvZnR3YXJlIEF0dGVz
-                dGF0aW9uIEludGVybWVkaWF0ZTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABOue
-                efhCY1msyyqRTImGzHCtkGaTgqlzJhP+rMv4ISdMIXSXSir+pblNf2bU4GUQZjW8
-                U7ego6ZxWD7bPhGuEBSjZjBkMB0GA1UdDgQWBBQ//KzWGrE6noEguNUlHMVlux6R
-                qTAfBgNVHSMEGDAWgBTIrel3TEXDo88NFhDkeUM6IVowzzASBgNVHRMBAf8ECDAG
-                AQH/AgEAMA4GA1UdDwEB/wQEAwIChDAKBggqhkjOPQQDAgNIADBFAiBLipt77oK8
-                wDOHri/AiZi03cONqycqRZ9pDMfDktQPjgIhAO7aAV229DLp1IQ7YkyUBO86fMy9
-                Xvsiu+f+uXc/WT/7
-                -----END CERTIFICATE-----
+           -----BEGIN EC PRIVATE KEY-----
+            MHcCAQEEIJOtdY1T+ZgDWvad48cw6Ey4j9k93PKJexFKr+XRRBOCoAoGCCqGSM49
+            AwEHoUQDQgAEeU14ILd16fsmbtAHT5Ay9Hs0Zb4wHKx+M0s55HaCSfiUpy07nj+S
+            hfXux5HRth1Sv83NvGKMIVuf8y2SKqlcbQ==
+           -----END EC PRIVATE KEY-----
                 """;
         public static final String CERTIFICATE_2 = """
-                -----BEGIN CERTIFICATE-----
-                MIICizCCAjKgAwIBAgIJAKIFntEOQ1tXMAoGCCqGSM49BAMCMIGYMQswCQYDVQQG
-                EwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNTW91bnRhaW4gVmll
-                dzEVMBMGA1UECgwMR29vZ2xlLCBJbmMuMRAwDgYDVQQLDAdBbmRyb2lkMTMwMQYD
-                VQQDDCpBbmRyb2lkIEtleXN0b3JlIFNvZnR3YXJlIEF0dGVzdGF0aW9uIFJvb3Qw
-                HhcNMTYwMTExMDA0MzUwWhcNMzYwMTA2MDA0MzUwWjCBmDELMAkGA1UEBhMCVVMx
-                EzARBgNVBAgMCkNhbGlmb3JuaWExFjAUBgNVBAcMDU1vdW50YWluIFZpZXcxFTAT
-                BgNVBAoMDEdvb2dsZSwgSW5jLjEQMA4GA1UECwwHQW5kcm9pZDEzMDEGA1UEAwwq
-                QW5kcm9pZCBLZXlzdG9yZSBTb2Z0d2FyZSBBdHRlc3RhdGlvbiBSb290MFkwEwYH
-                KoZIzj0CAQYIKoZIzj0DAQcDQgAE7l1ex+HA220Dpn7mthvsTWpdamguD/9/SQ59
-                dx9EIm29sa/6FsvHrcV30lacqrewLVQBXT5DKyqO107sSHVBpKNjMGEwHQYDVR0O
-                BBYEFMit6XdMRcOjzw0WEOR5QzohWjDPMB8GA1UdIwQYMBaAFMit6XdMRcOjzw0W
-                EOR5QzohWjDPMA8GA1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQDAgKEMAoGCCqG
-                SM49BAMCA0cAMEQCIDUho++LNEYenNVg8x1YiSBq3KNlQfYNns6KGYxmSGB7AiBN
-                C/NR2TB8fVvaNTQdqEcbY6WFZTytTySn502vQX3xvw==
-                -----END CERTIFICATE-----
+           -----BEGIN CERTIFICATE REQUEST-----
+            MIIBeTCCAR8CAQAwgYoxCzAJBgNVBAYTAkNOMQ4wDAYDVQQIDAVIZW5hbjEPMA0G
+            A1UEBwwGQW55YW5nMRMwEQYDVQQKDApDaHVuIEd1YW5nMRMwEQYDVQQLDApDaHVu
+            IEd1YW5nMQ8wDQYDVQQDDAZtaS5jb20xHzAdBgkqhkiG9w0BCQEWEDk5NjE5MTQ0
+            MUBxcS5jb20wWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAAR5TXggt3Xp+yZu0AdP
+            kDL0ezRlvjAcrH4zSznkdoJJ+JSnLTueP5KF9e7HkdG2HVK/zc28YowhW5/zLZIq
+            qVxtoDIwFQYJKoZIhvcNAQkHMQgMBjEyMzQ1NjAZBgkqhkiG9w0BCQIxDAwKQ2h1
+            biBHdWFuZzAKBggqhkjOPQQDAgNIADBFAiEA8LJpQnwUGnFCtu0ZL38xcmHA4Kmh
+            3e9Yq0pWf4w0iMECIChDm/1g3GnYXZNs7djt6Oi9jAQ0Zlu3glQbUUtDkvsF
+            -----END CERTIFICATE REQUEST-----
                 """;
+        public static final String CERTIFICATE_3 = """
+                -----BEGIN EC PARAMETERS-----
+BggqhkjOPQMBBw==
+-----END EC PARAMETERS-----
+                """;
+
     }
 
     public static final class RSA {
         public static final String PRIVATE_KEY = """
-                -----BEGIN RSA PRIVATE KEY-----
-                MIICXQIBAAKBgQDAgyPcVogbuDAgafWwhWHG7r5/BeL1qEIEir6LR752/q7yXPKb
-                KvoyABQWAUKZiaFfz8aBXrNjWDwv0vIL5Jgyg92BSxbX4YVBeuVKvClqOm21wAQI
-                O2jFVsHwIzmRZBmGTVC3TUCuykhMdzVsiVoMJ1q/rEmdXX0jYvKcXgLocQIDAQAB
-                AoGBAL6GCwuZqAKm+xpZQ4p7txUGWwmjbcbpysxr88AsNNfXnpTGYGQo2Ix7f2V3
-                wc3qZAdKvo5yht8fCBHclygmCGjeldMu/Ja20IT/JxpfYN78xwPno45uKbqaPF/C
-                woB2tqiWrx0014gozpvdsfNPnJQEQweBKY4gExZyW728mTpBAkEA4cbZJ2RsCRbs
-                NoJtWUmDdAwh8bB0xKGlmGfGaXlchdPcRkxbkp6Uv7NODcxQFLEPEzQat/3V9gQU
-                0qMmytQcxQJBANpIWZd4XNVjD7D9jFJU+Y5TjhiYOq6ea35qWntdNDdVuSGOvUAy
-                DSg4fXifdvohi8wti2il9kGPu+ylF5qzr70CQFD+/DJklVlhbtZTThVFCTKdk6PY
-                ENvlvbmCKSz3i9i624Agro1X9LcdBThv/p6dsnHKNHejSZnbdvjl7OnA1J0CQBW3
-                TPJ8zv+Ls2vwTZ2DRrCaL3DS9EObDyasfgP36dH3fUuRX9KbKCPwOstdUgDghX/y
-                qAPpPu6W1iNc6VRCvCECQQCQp0XaiXCyzWSWYDJCKMX4KFb/1mW6moXI1g8bi+5x
-                fs0scurgHa2GunZU1M9FrbXx8rMdn4Eiz6XxpVcPmy0l
-                -----END RSA PRIVATE KEY-----
+              -----BEGIN EC PRIVATE KEY-----
+MHcCAQEEIJAqCmHeg1QievBEMDtSwJpyAee1qMQLC8XkAn49p1sFoAoGCCqGSM49
+AwEHoUQDQgAEw/y94ahqXwuRgrO7zciYDgizikmmgjcEzymPIT5TMI0gxIOk9yZ9
+x9FJN2OqSM74U89h7m7jPvkWXrOzqrHLuw==
+-----END EC PRIVATE KEY-----
                 """;
         public static final String CERTIFICATE_1 = """
-                -----BEGIN CERTIFICATE-----
-                MIICtjCCAh+gAwIBAgICEAAwDQYJKoZIhvcNAQELBQAwYzELMAkGA1UEBhMCVVMx
-                EzARBgNVBAgMCkNhbGlmb3JuaWExFjAUBgNVBAcMDU1vdW50YWluIFZpZXcxFTAT
-                BgNVBAoMDEdvb2dsZSwgSW5jLjEQMA4GA1UECwwHQW5kcm9pZDAeFw0xNjAxMDQx
-                MjQwNTNaFw0zNTEyMzAxMjQwNTNaMHYxCzAJBgNVBAYTAlVTMRMwEQYDVQQIDApD
-                YWxpZm9ybmlhMRUwEwYDVQQKDAxHb29nbGUsIEluYy4xEDAOBgNVBAsMB0FuZHJv
-                aWQxKTAnBgNVBAMMIEFuZHJvaWQgU29mdHdhcmUgQXR0ZXN0YXRpb24gS2V5MIGf
-                MA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDAgyPcVogbuDAgafWwhWHG7r5/BeL1
-                qEIEir6LR752/q7yXPKbKvoyABQWAUKZiaFfz8aBXrNjWDwv0vIL5Jgyg92BSxbX
-                4YVBeuVKvClqOm21wAQIO2jFVsHwIzmRZBmGTVC3TUCuykhMdzVsiVoMJ1q/rEmd
-                XX0jYvKcXgLocQIDAQABo2YwZDAdBgNVHQ4EFgQU1AwQG/jNY7n3OVK1DhNcpteZ
-                k4YwHwYDVR0jBBgwFoAUKfrxrMxN0kyWQCd1trDpMuUH/i4wEgYDVR0TAQH/BAgw
-                BgEB/wIBADAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADgYEAni1IX4xn
-                M9waha2Z11Aj6hTsQ7DhnerCI0YecrUZ3GAi5KVoMWwLVcTmnKItnzpPk2sxixZ4
-                Fg2Iy9mLzICdhPDCJ+NrOPH90ecXcjFZNX2W88V/q52PlmEmT7K+gbsNSQQiis6f
-                9/VCLiVE+iEHElqDtVWtGIL4QBSbnCBjBH8=
-                -----END CERTIFICATE-----
+                -----BEGIN CERTIFICATE REQUEST-----
+MIIBgDCCAScCAQAwgZIxCzAJBgNVBAYTAkNOMQ4wDAYDVQQIDAVIZW5hbjEPMA0G
+A1UEBwwGQW55YW5nMRMwEQYDVQQKDApDaHVuIEd1YW5nMRMwEQYDVQQLDApDaHVu
+IEd1YW5nMRcwFQYDVQQDDA53d3cudmlpaWlzLmNvbTEfMB0GCSqGSIb3DQEJARYQ
+OTk2MTkxNDQxQHFxLmNvbTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABMP8veGo
+al8LkYKzu83ImA4Is4pJpoI3BM8pjyE+UzCNIMSDpPcmfcfRSTdjqkjO+FPPYe5u
+4z75Fl6zs6qxy7ugMjAVBgkqhkiG9w0BCQcxCAwGMTIzNDU2MBkGCSqGSIb3DQEJ
+AjEMDApDaHVuIEd1YW5nMAoGCCqGSM49BAMCA0cAMEQCIH06sCF6+vNgBvcDmO4b
+OwfLT3vxpFSgwOEtbEZctfmDAiBWM+Y4gNAfYw3iuKaUE9Stna1TG7lqtmRhNSXx
+XVPPWw==
+-----END CERTIFICATE REQUEST-----
                 """;
         public static final String CERTIFICATE_2 = """
                 -----BEGIN CERTIFICATE-----
-                MIICpzCCAhCgAwIBAgIJAP+U2d2fB8gMMA0GCSqGSIb3DQEBCwUAMGMxCzAJBgNV
-                BAYTAlVTMRMwEQYDVQQIDApDYWxpZm9ybmlhMRYwFAYDVQQHDA1Nb3VudGFpbiBW
-                aWV3MRUwEwYDVQQKDAxHb29nbGUsIEluYy4xEDAOBgNVBAsMB0FuZHJvaWQwHhcN
-                MTYwMTA0MTIzMTA4WhcNMzUxMjMwMTIzMTA4WjBjMQswCQYDVQQGEwJVUzETMBEG
-                A1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNTW91bnRhaW4gVmlldzEVMBMGA1UE
-                CgwMR29vZ2xlLCBJbmMuMRAwDgYDVQQLDAdBbmRyb2lkMIGfMA0GCSqGSIb3DQEB
-                AQUAA4GNADCBiQKBgQCia63rbi5EYe/VDoLmt5TRdSMfd5tjkWP/96r/C3JHTsAs
-                Q+wzfNes7UA+jCigZtX3hwszl94OuE4TQKuvpSe/lWmgMdsGUmX4RFlXYfC78hdL
-                t0GAZMAoDo9Sd47b0ke2RekZyOmLw9vCkT/X11DEHTVm+Vfkl5YLCazOkjWFmwID
-                AQABo2MwYTAdBgNVHQ4EFgQUKfrxrMxN0kyWQCd1trDpMuUH/i4wHwYDVR0jBBgw
-                FoAUKfrxrMxN0kyWQCd1trDpMuUH/i4wDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8B
-                Af8EBAMCAoQwDQYJKoZIhvcNAQELBQADgYEAT3LzNlmNDsG5dFsxWfbwjSVJMJ6j
-                HBwp0kUtILlNX2S06IDHeHqcOd6os/W/L3BfRxBcxebrTQaZYdKumgf/93y4q+uc
-                DyQHXrF/unlx/U1bnt8Uqf7f7XzAiF343ZtkMlbVNZriE/mPzsF83O+kqrJVw4Op
-                Lvtc9mL1J1IXvmM=
-                -----END CERTIFICATE-----
+MIICSTCCAe6gAwIBAgITKQGVhF7eb4QXEq191xP06HFiODAKBggqhkjOPQQDAjCB
+kjELMAkGA1UEBhMCQ04xDjAMBgNVBAgMBUhlbmFuMQ8wDQYDVQQHDAZBbnlhbmcx
+EzARBgNVBAoMCkNodW4gR3VhbmcxEzARBgNVBAsMCkNodW4gR3VhbmcxFzAVBgNV
+BAMMDnd3dy52aWlpaXMuY29tMR8wHQYJKoZIhvcNAQkBFhA5OTYxOTE0NDFAcXEu
+Y29tMB4XDTI0MDYwNDExMTc1NVoXDTI1MDYwNDExMTc1NVowgZIxCzAJBgNVBAYT
+AkNOMQ4wDAYDVQQIDAVIZW5hbjEPMA0GA1UEBwwGQW55YW5nMRMwEQYDVQQKDApD
+aHVuIEd1YW5nMRMwEQYDVQQLDApDaHVuIEd1YW5nMRcwFQYDVQQDDA53d3cudmlp
+aWlzLmNvbTEfMB0GCSqGSIb3DQEJARYQOTk2MTkxNDQxQHFxLmNvbTBZMBMGByqG
+SM49AgEGCCqGSM49AwEHA0IABMP8veGoal8LkYKzu83ImA4Is4pJpoI3BM8pjyE+
+UzCNIMSDpPcmfcfRSTdjqkjO+FPPYe5u4z75Fl6zs6qxy7ujITAfMB0GA1UdDgQW
+BBR0nMhX+QceAUK6pRIJ3bdDinUo8jAKBggqhkjOPQQDAgNJADBGAiEAomNh37Uz
+J+rknFTm8Cc0DQo8uB2BHD8MZ8MZXfy3VJICIQCNVH7VpAJyhBnlFfVARfhNZhSY
+r2hTMoU0qqEzhOeOIA==
+-----END CERTIFICATE-----
                 """;
+                
+        public static final String CERTIFICATE_3 = """
+            -----BEGIN CERTIFICATE-----
+MIICYjCCAgigAwIBAgIUEpviGr1GXTKedZWg71dZtRN0CoIwCgYIKoZIzj0EAwIw
+gZIxCzAJBgNVBAYTAkNOMQ4wDAYDVQQIDAVIZW5hbjEPMA0GA1UEBwwGQW55YW5n
+MRMwEQYDVQQKDApDaHVuIEd1YW5nMRMwEQYDVQQLDApDaHVuIEd1YW5nMRcwFQYD
+VQQDDA53d3cudmlpaWlzLmNvbTEfMB0GCSqGSIb3DQEJARYQOTk2MTkxNDQxQHFx
+LmNvbTAeFw0yNDA2MDQxMTI0MTlaFw0yNTA2MDQxMTI0MTlaMIGKMQswCQYDVQQG
+EwJDTjEOMAwGA1UECAwFSGVuYW4xDzANBgNVBAcMBkFueWFuZzETMBEGA1UECgwK
+Q2h1biBHdWFuZzETMBEGA1UECwwKQ2h1biBHdWFuZzEPMA0GA1UEAwwGbWkuY29t
+MR8wHQYJKoZIhvcNAQkBFhA5OTYxOTE0NDFAcXEuY29tMFkwEwYHKoZIzj0CAQYI
+KoZIzj0DAQcDQgAEeU14ILd16fsmbtAHT5Ay9Hs0Zb4wHKx+M0s55HaCSfiUpy07
+nj+ShfXux5HRth1Sv83NvGKMIVuf8y2SKqlcbaNCMEAwHQYDVR0OBBYEFPhK7uMs
+cN0vo0TyqmraT+DQLoqnMB8GA1UdIwQYMBaAFHScyFf5Bx4BQrqlEgndt0OKdSjy
+MAoGCCqGSM49BAMCA0gAMEUCIBQDxiUC8A7aKNmtBZmovEC2HS6m9EBsoWza/Phl
+USuEAiEA7WAwZfUbngRNaigewd2YmYbmRixuXWfHoAe8XBpJ7vY=
+-----END CERTIFICATE-----
+            """;
     }
 }
